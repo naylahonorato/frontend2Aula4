43a4ca9969ac0ad0d2904969b6fd5ddbeafbd6a1
Nayla Honorato <naylahonorato@hotmail.com>
2021-08-18 12:04:58 -0300

    Aula4

---
+++ (WrkTb) Aula 04/app.js
@@ -0,0 +1,15 @@
+let elementBody = document.querySelector('body');
+console.log(elementBody);
+
+let elementH1 = document.querySelector('h1');
+console.log(elementH1);
+
+let elementContent = document.querySelector('conteudo');
+console.log(elementContent);
+
+let elementSection = document.querySelector('section');
+console.log(elementSection);
+
+let elementP = document.querySelectorAll('p');
+console.log(elementP);  
+
